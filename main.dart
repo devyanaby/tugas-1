84132cb0f2a29d6f2c9ae560482c384b61df9920
devyanaby <135807002+devyanaby@users.noreply.github.com>
2023-09-14 14:38:48 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,13 @@
-class Mahasiswa {
-  String nim;
-  String nama;
-  int tahunLahir;
-
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
     print("Perkenalkan, nama saya $nama");
+    print(
+        "Perkenalkan, nama saya $nama dengan nim $nim dan usia saya ${DateTime.now().year - tahunLahir} tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021058", "Devyan Aby Rifai", 2002);
+  mhs.perkenalan();
 }
